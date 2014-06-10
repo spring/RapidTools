4d66f7d839d793bbe801e311b7fa7fb5d3268f7f
abma <spring@abma.de>
2014-06-10 14:36:27 +0200

    add update-apache-config.sh

---
+++ Scripts/update-apache-config.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+DOMAINS=$(find /home/packages/git -maxdepth 1 -mindepth 1 -type d)
+
+for DOMAIN in $DOMAINS; do
+
+TAG=$(basename $DOMAIN)
+
+# repo / tag is changed to fix order of files
+FILE=/etc/apache2/sites-enabled/001-$TAG-repo.springrts.com
+echo "Creating $FILE"
+cat >$FILE << EOF
+
+#Automaticly created with $(pwd)/$0, don't edit!
+<VirtualHost *:80>
+    ServerName $TAG-repo.springrts.com
+    ServerAdmin chris@springrts.com
+    ErrorLog /var/log/apache2/$TAG-repo.springrts.com-error.log
+    CustomLog /var/log/apache2/$TAG-repo.springrts.com-access.log combined
+    DocumentRoot /home/packages/www/$TAG
+
+    <Location />
+        RewriteEngine on
+        RewriteRule streamer.cgi$ nph-streamer2.cgi
+        AddHandler cgi-script .cgi
+        Options +ExecCGI
+    </Location>
+</VirtualHost>
+EOF
+
+done
