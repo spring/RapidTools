97edeecb648725ba1f8eb31c653259ea0155fcda
abma <spring@abma.de>
2014-06-13 20:28:56 +0200

    use tag.repo.springrts.com as domain name

--- Scripts/update-apache-config.sh
+++ Scripts/update-apache-config.sh
@@ -11,16 +11,16 @@ for DOMAIN in $DOMAINS; do
 TAG=$(basename $DOMAIN)
 
 # 001- is prefixed for defined order of files
-FILE=/etc/apache2/sites-enabled/001-$TAG-repo.springrts.com
+FILE=/etc/apache2/sites-enabled/001-$TAG.repo.springrts.com
 echo "Creating $FILE"
 cat >$FILE << EOF
 
 #Automaticly created with $(pwd)/$0, don't edit!
 <VirtualHost *:80>
-    ServerName $TAG-repo.springrts.com
+    ServerName $TAG.repo.springrts.com
     ServerAdmin chris@springrts.com
-    ErrorLog /var/log/apache2/$TAG-repo.springrts.com-error.log
-    CustomLog /var/log/apache2/$TAG-repo.springrts.com-access.log combined
+    ErrorLog /var/log/apache2/$TAG.repo.springrts.com-error.log
+    CustomLog /var/log/apache2/$TAG.repo.springrts.com-access.log combined
     DocumentRoot $WWWROOT/$TAG
 
     <Location />
