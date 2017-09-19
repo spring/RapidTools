0b7fbeb34ebcaed2469c2cf1b378e9de7eab5450
abma <spring@abma.de>
2017-09-20 01:42:40 +0200

    wait for logininfo before joining / sending data

--- Scripts/loggit.py
+++ Scripts/loggit.py
@@ -17,17 +17,26 @@ password = passwords.password
 
 channels = set(sys.argv[1:])
 
+buf = ""
 template = "JOIN $CHANNEL$\n"
 for line in sys.stdin:
 	template += "SAY $CHANNEL$ " + line + "\n"
-buf = 'LOGIN ' + username + ' ' + password + ' 0 * TASClient 0.33\t0\tcl sp p\n'
 for channel in channels:
 	buf += template.replace("$CHANNEL$", channel)
 buf += "EXIT Thanks for using rapid! https://github.com/spring/RapidTools\n"
 
 socket = socket.socket()
-socket.settimeout(2.0)
+socket.settimeout(5.0)
 socket.connect(('lobby.springrts.com', 8200))
+
+socket.sendall('LOGIN ' + username + ' ' + password + ' 0 * TASClient 0.33\t0\tcl sp p\n')
+
+
+for i in range(0, 30):
+	data = socket.recv(1024)
+	if data and data.lower().find("logininfoend") >= 0:
+		break
+
 socket.sendall(buf)
 
 time.sleep(1.0)
